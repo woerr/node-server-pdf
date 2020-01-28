d07003e1c58f29053cffca1537b6b75f14553141
RomanNovo <26027164+RomanNovo@users.noreply.github.com>
2020-01-28 11:32:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.use(bodyParser.json({limit: '50mb'})); // for parsing application/json
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
